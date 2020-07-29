613bc5b9b5d4708027381f01c3ead7d167a22958
niconiahi <nicolas.accetta@gmail.com>
2020-07-29 00:07:58 -0300

    refactor: Remove unused util

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,2 +1,1 @@
 export const safelyGetObject = (key: string) => JSON.parse(localStorage.getItem(key) || '{}');
-export const safelyGetBoolean = (key: string) => Boolean(localStorage.getItem(key)) || false;
